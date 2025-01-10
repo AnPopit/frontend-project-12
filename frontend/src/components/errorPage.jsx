0636134aefbdfaa6e42100f388ff43fbd1a5bc72
Politova <anna_loginovskikh@mail.ru>
2025-01-10 15:10:06 +0500

    2 step fix

--- frontend/src/components/errorPage.jsx
+++ frontend/src/components/errorPage.jsx
@@ -1,8 +1,18 @@
+import errorImg from '../assets/error.svg';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    Navigate,
+    useLocation,
+  } from 'react-router-dom';
+
 const ErrorPage = () => {
     return (
-        <div className="text-center"><img alt="Страница не найдена" className="img-fluid h-25" width="500" height="600" src="src/assets/error.svg"/>
+        <div className="text-center"><img alt="Страница не найдена" className="img-fluid h-25" width="500" height="600" src={errorImg}/>
             <h1 className="h4 text-muted">Страница не найдена</h1>
-            <p className="text-muted">Но вы можете перейти <a href="/">на главную страницу</a></p>
+            <p className="text-muted">Но вы можете перейти <Link to="/">на главную страницу</Link></p>
             </div> 
     )
   };
