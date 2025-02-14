ff20ca1c525e672179d90cffcbf211bb2f632a96
Politova <anna_loginovskikh@mail.ru>
2025-02-14 14:32:17 +0500

    fix

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,13 +2,12 @@ import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import init from './init.jsx';
 
-
 const app = async () => {
-    const chat = document.getElementById('chat');
-    const root = createRoot(chat);
-    root.render(
-        await init()
-    );
+  const chat = document.getElementById('chat');
+  const root = createRoot(chat);
+  root.render(
+    await init(),
+  );
 };
 
 app();
--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,61 +1,60 @@
-/* eslint-disable functional/no-expression-statement */
-
 import React from 'react';
 import i18next from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import { initReactI18next, I18nextProvider } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import resources from './locales/index.js';
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
 import { Provider } from 'react-redux';
+import resources from './locales/index.js';
 import store from './slices/index.js';
-import { I18nextProvider } from 'react-i18next';
+
 import Component from './components/App.jsx';
 import { setMessages } from './slices/messagesSlice.js';
-import { setChannel, delChannel, updateChannel, addChannel } from './slices/channelsSlice.js';
+import {
+  addChannel,
+} from './slices/channelsSlice.js';
+// import {
+// setChannel, delChannel, updateChannel, addChannel,
+// } from './slices/channelsSlice.js';
 
 const init = async () => {
-    const i18n = i18next.createInstance();
-    await i18n
-        .use(initReactI18next)
-        .use(LanguageDetector)
-        .init({
-            resources,
-            fallbackLng: 'ru',
-            interpolation: {
-                escapeValue: false,
-            },
-        });
+  const i18n = i18next.createInstance();
+  await i18n
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .init({
+      resources,
+      fallbackLng: 'ru',
+      interpolation: {
+        escapeValue: false,
+      },
+    });
 
+  const socket = io();
+  socket.on('newMessage', (payload) => {
+    store.dispatch(setMessages(payload));
+  });// добавить сокеты на действия с каналами
 
-    const socket = io()
-    socket.on('newMessage', (payload) => {
-        store.dispatch(setMessages(payload))
-    })//добавить сокеты на действия с каналами
+  socket.on('newChannel', (payload) => {
+    store.dispatch(addChannel(payload));
+  });
 
-    socket.on('newChannel', (payload) => {
-        store.dispatch(addChannel(payload))   
-      console.log({payload}) // { id: 6, name: "new channel", removable: true }
-   });
-
-    //socket.on('removeChannel', (payload) => {
-     //   store.dispatch(delChannel(payload))   
-     //   console.log(payload); // { id: 6 };
+  // socket.on('removeChannel', (payload) => {
+  //   store.dispatch(delChannel(payload))
+  //   console.log(payload); // { id: 6 };
   //  });
 
-   // socket.on('renameChannel', (payload) => {
-   //     store.dispatch(updateChannel(payload))   
-   //     console.log(payload); // { id: 7, name: "new name channel", removable: true }
-  //  }); 
+  // socket.on('renameChannel', (payload) => {
+  //     store.dispatch(updateChannel(payload))
+  //     console.log(payload); // { id: 7, name: "new name channel", removable: true }
+  //  });
 
-
-
-    return (
-        <Provider store={store}>
-            <I18nextProvider i18n={i18n}>
-                <Component />
-            </I18nextProvider>
-        </Provider>
-    )
-}
+  return (
+    <Provider store={store}>
+      <I18nextProvider i18n={i18n}>
+        <Component />
+      </I18nextProvider>
+    </Provider>
+  );
+};
 
 export default init;
